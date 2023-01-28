0c587d97bae65791c1eaafe85b9d894a282cdd41
Christopher Couret <christophercouret@Christophers-iMac.local>
2023-01-28 12:58:32 -0500

    update 3 server

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codecouret.onrender.com',{
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
